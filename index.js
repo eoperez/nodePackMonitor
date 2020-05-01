c7baa80d730ea9bebeed7e73a32ef37191aee591
Edwin Perez <Edwin.Perez@blackboard.com>
2020-05-01 14:11:42 -0400

    Trying to make the PI port to work.

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 // Software Configuration. 
 //TODO: need to externalize this so I can save to a file
-// PI port: /dev/ttyS0
-// Mac port: /dev/cu.SLAB_USBtoUART
+
+// Default Port
+const defaultPort = '/dev/ttyS0' // PI Port
+// const defaultPort = '/dev/cu.SLAB_USBtoUART' // Mac port
+// Configuration
 const configuration = {
-    commPort: "/dev/ttyS0",
-    calibration: 517,
-    voltageTolerance: 10,
-    serverPort: 3030,
-    sample: 40,
-    availablePorts: []
+  commPort: defaultPort,
+  calibration: 517,
+  voltageTolerance: 10,
+  serverPort: 3030,
+  sample: 40,
+  availablePorts: []
 }
 
 const Serialport = require('serialport');
 const Readline = require('@serialport/parser-readline');
-const {crc8} = require('crc');
+const { crc8 } = require('crc');
 
 /********************** MONITOR CODE ***************************** */
 /* 
@@ -27,17 +30,17 @@ const PACKET_LENGTH = 4;
 
 // Packet containing payload to assemble the request to monitors
 let packet = {
-    address: ADDRESS_BROADCAST, // Address of the monitor, use 0 for broadcast.
-    reg: REG_ADDRESS, // command registry: supported by monitor: address registration = 1 (0x1), Voltage request = 3 (0x3) and temperature request = 4 (0x4)
-    request: true, // true if is a request, false is a response.
-    value: 4200, // value in will use 2 bytes
-    write: true // if is a write (true) or read (false)
+  address: ADDRESS_BROADCAST, // Address of the monitor, use 0 for broadcast.
+  reg: REG_ADDRESS, // command registry: supported by monitor: address registration = 1 (0x1), Voltage request = 3 (0x3) and temperature request = 4 (0x4)
+  request: true, // true if is a request, false is a response.
+  value: 4200, // value in will use 2 bytes
+  write: true // if is a write (true) or read (false)
 }
 
 function debugAsBinary(number) {
-    let binary = number.toString(2);
-    binary = "00000000".substr(binary.length) + binary;
-    console.log(binary);
+  let binary = number.toString(2);
+  binary = "00000000".substr(binary.length) + binary;
+  console.log(binary);
 }
 
 /*
@@ -51,37 +54,59 @@ function debugAsBinary(number) {
 
 // Encodes a packet into a array 
 function encode(packet) {
-    // return an array (buffer) with 4 bytes build as described above.
-    return [
-        (packet.address << 1) | (packet.request? 1 : 0),
-        (packet.reg << 1) | (packet.write? 1 : 0),
-        packet.value >> 8, // 8 bits 
-        packet.value & 0xFF // rest of the 8 bits
-    ]
+  // return an array (buffer) with 4 bytes build as described above.
+  return [
+    (packet.address << 1) | (packet.request ? 1 : 0),
+    (packet.reg << 1) | (packet.write ? 1 : 0),
+    packet.value >> 8, // 8 bits 
+    packet.value & 0xFF // rest of the 8 bits
+  ]
 }
 
 // decodes buffer into a packet
 function decode(buffer) {
-    // return an packet object with data included in the buffer
-    return {
-        address: buffer[0] >> 1,
-        request: buffer[0] & 1,
-        reg: buffer[1] >> 1,
-        write: buffer[1] & 1,
-        value: (buffer[2] << 8) | buffer[3]
-    }
+  // return an packet object with data included in the buffer
+  return {
+    address: buffer[0] >> 1,
+    request: buffer[0] & 1,
+    reg: buffer[1] >> 1,
+    write: buffer[1] & 1,
+    value: (buffer[2] << 8) | buffer[3]
+  }
 }
 
 /********************** SERIAL CODE ***************************** */
 
 // Get a list of serial ports
 Serialport.list().then((portList, error) => {
-    configuration.availablePorts = portList;
-    console.log('Available Ports:', portList);
+  configuration.availablePorts = portList;
+  console.log('Available Ports:', portList);
 });
 
-const port = new Serialport(configuration.commPort,{ baudRate: 9600 }); // instance of the port
+const port = new Serialport(configuration.commPort, { baudRate: 9600 }); // instance of the port
 
-const parser = new Readline();
-port.pipe(parser);
-parser.on('data', line => console.log(`> ${line}`));
+port.on('open', function () {
+  console.log('port opened');
+
+  const parser = new Readline();
+  port.pipe(parser);
+  console.log(`listening port: ${configuration.port}`)
+  parser.on('data', line => console.log(`> ${line}`));
+
+});
+
+port.on('error', function (err) {
+  console.error('Hmm..., error!');
+  console.error(err);
+  process.exit(1);
+});
+
+function sendMessage() {
+  port.write('running\n', function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Message sent successfully');
+    }
+  });
+}
