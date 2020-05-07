d833db3e768f361ec25337710b574871ac807655
Edwin Perez <Edwin.Perez@blackboard.com>
2020-05-07 13:04:04 -0400

    stripped CRC check.

--- index.js
+++ index.js
@@ -116,12 +116,7 @@ port.on('error', function (err) {
 });
 
 function responseHandler(data){
-  debugAsBinary(data[4]);
   response = decode(data);
-  const crcReceived = crc8(data, PACKET_LENGTH);
-  Buffer.(crcReceived.toString());
-  debugAsBinary(crcReceived);
-  console.log((crcReceived == data[4]));
   switch (response.reg) {
       case REG_VOLTAGE:
         // TODO: emit to socket type "voltage" {pack:ADDRESS, value: VALUE}
