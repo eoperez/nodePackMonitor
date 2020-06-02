b257df006ab49734ee13d70b0137c0541d41f0cd
Edwin Perez <Edwin.Perez@blackboard.com>
2020-06-02 18:52:35 -0400

    adding port

--- src/server/services/Pip5048MgMonitor.Service.js
+++ src/server/services/Pip5048MgMonitor.Service.js
@@ -5,7 +5,7 @@ const QPIGS = [0X51,0X50,0X49,0X47,0X53,0XB7,0XA9,0X0D];
 SerialPort.list().then((commPorts) => {
     console.log(commPorts);
 });
-const port = new SerialPort('ertewrwert', {baudRate: 2400});
+const port = new SerialPort('/dev/tty.usbserial-AL065TDA', {baudRate: 2400});
 
 const parser = port.pipe(new SerialPort.parsers.Readline({delimiter: '\r'}));
 
