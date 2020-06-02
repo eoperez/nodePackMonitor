3cc57f07a7c4721ad21dcf9dce91312ee762dc5d
Edwin Perez <Edwin.Perez@blackboard.com>
2020-06-02 18:50:14 -0400

    testing PIP connection

--- src/server/services/Pip5048MgMonitor.Service.js
+++ src/server/services/Pip5048MgMonitor.Service.js
@@ -7,12 +7,13 @@ SerialPort.list().then((commPorts) => {
 });
 const port = new SerialPort('ertewrwert', {baudRate: 2400});
 
-/*
-
 const parser = port.pipe(new SerialPort.parsers.Readline({delimiter: '\r'}));
 
 port.on('open', () => {
     console.log('Port open.')
     port.write([0X51,0X50,0X49,0X47,0X53,0XB7,0XA9,0X0D]);
 });
-*/
+
+port.on('data', (data) => {
+    console.log('Data:', data)
+})
