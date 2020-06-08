d6e9d8a2d9b120a450e65c185160e03f73b39ac4
Edwin Perez <Edwin.Perez@blackboard.com>
2020-06-08 18:15:45 -0400

    state defaults and wiring dynamic values

--- src/client/src/components/Dashboard.tsx
+++ src/client/src/components/Dashboard.tsx
@@ -8,11 +8,13 @@ import {
 import BarChart from './BarChart'
 import GaugeTile from "./GaugeTile";
 import socketIOClient from "socket.io-client"
+
 const ENDPOINT = "http://192.168.0.6:5000"
 
 interface Props {
 
 }
+
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -36,7 +38,47 @@ const useStyles = makeStyles((theme: Theme) =>
 export default function Dashboard({}: Props): ReactElement {
     const classes = useStyles();
     const [bars, setBars] = useState([]);
-    const [inverter,setInverter] = useState({});
+    const [inverter,setInverter] = useState({
+      grid: {
+          voltage: '--- ',
+          frequency: '--- ',
+          power: 0,
+          loadPercent: 0
+      },
+      pv: {
+          currentBattery: '--- ',
+          voltage_1: '--- ',
+          chargingPower: '--- ',
+          powerForLoads: 0,
+          productionPercent: 0
+      },
+      consumption: {
+          voltage: '--- ',
+          frequency: '--- ',
+          powerVa: '--- ',
+          activePower: '--- ',
+          loadPercent: 0,
+          current: 0
+      },
+      battery: {
+          voltage: '--- ',
+          chargingCurrent: '--- ',
+          capacity: 0,
+          voltageFromScc: '--- ',
+          dischargeCurrent: '--- ',
+          powerOut: 0,
+          powerIn: 0
+      },
+      inverter: {
+          busVoltage: '--- ',
+          heatSinkTemperature: '--- ',
+          deviceStatus: {
+              chargingAC: '',
+              chargingSccAcc: '',
+              chargingScc: ''
+          }
+      }
+  });
 
     useEffect(() => {
       const socket = socketIOClient(ENDPOINT);
@@ -54,45 +96,45 @@ export default function Dashboard({}: Props): ReactElement {
         <Grid container spacing={1}>
           <Grid item xs={3}>
             <Paper className={classes.paper}>
-               <GaugeTile id= "gridInfo" title="GRID" percentage={.5}
+               <GaugeTile id= "gridInfo" title="GRID" percentage={inverter.grid.loadPercent}
                infoTiles={[
-                 {primaryText: "250v", secondaryText: "Voltage"},
-                 {primaryText: "60hz", secondaryText: "Frequency"},
-                 {primaryText: "300w", secondaryText: "Power"}
+                 {primaryText: inverter.grid.voltage + "V", secondaryText: "Voltage"},
+                 {primaryText: inverter.grid.frequency + "Hz", secondaryText: "Frequency"},
+                 {primaryText: inverter.grid.power + "W", secondaryText: "Power"}
                 ]} />
             </Paper>
           </Grid>
           <Grid item xs={3}>
             <Paper className={classes.paper}>
-              <GaugeTile id= "pvInfo" title="PV" percentage={.5} 
+              <GaugeTile id= "pvInfo" title="PV" percentage={inverter.pv.productionPercent} 
               infoTiles={[
-                {primaryText: "250v", secondaryText: "Voltage"},
-                {primaryText: "16a", secondaryText: "Battery Current"},
-                {primaryText: "200w", secondaryText: "Charging Power"},
-                {primaryText: "200w", secondaryText: "Power for Load"},
+                {primaryText: inverter.pv.voltage_1 + "V", secondaryText: "Voltage"},
+                {primaryText: inverter.pv.currentBattery + "A", secondaryText: "Battery Current"},
+                {primaryText: inverter.pv.chargingPower + "W", secondaryText: "Charging Power"},
+                {primaryText: inverter.pv.powerForLoads + "W", secondaryText: "Power for Load"},
               ]} reverse={true}/>
 
             </Paper>
           </Grid>
           <Grid item xs={3}>
             <Paper className={classes.paper}>
-              <GaugeTile id="consumption" title="Consumption" percentage={.5}
+              <GaugeTile id="consumption" title="Consumption" percentage={inverter.consumption.loadPercent}
               infoTiles={[
-                {primaryText: "250v", secondaryText: "Voltage"},
-                {primaryText: "60hz", secondaryText: "Frequency"},
-                {primaryText: "800va", secondaryText: "Power"},
-                {primaryText: "750w", secondaryText: "Active"}
+                {primaryText: inverter.consumption.voltage + "V", secondaryText: "Voltage"},
+                {primaryText: inverter.consumption.frequency + "Hz", secondaryText: "Frequency"},
+                {primaryText: inverter.consumption.powerVa + "Va", secondaryText: "Power"},
+                {primaryText: inverter.consumption.activePower + "W", secondaryText: "Active"}
               ]} />
             </Paper>
           </Grid>
           <Grid item xs={3}>
             <Paper className={classes.paper}>
-              <GaugeTile id="battery" title="Battery Bank" percentage={.5}
+              <GaugeTile id="battery" title="Battery Bank" percentage={inverter.battery.capacity}
                 infoTiles={[
-                  {primaryText: "0w", secondaryText: "Power In"},
-                  {primaryText: "0a", secondaryText: "Charging Current"},
-                  {primaryText: "56.7v", secondaryText: "Voltage"},
-                  {primaryText: "0w", secondaryText: "Power Out"}
+                  {primaryText: inverter.battery.powerIn + "W", secondaryText: "Power In"},
+                  {primaryText: inverter.battery.chargingCurrent + "A", secondaryText: "Charging Current"},
+                  {primaryText: inverter.battery.voltage + "V", secondaryText: "Voltage"},
+                  {primaryText: inverter.battery.powerOut + "W", secondaryText: "Power Out"}
                 ]} reverse={true}/>
             </Paper>
           </Grid>
