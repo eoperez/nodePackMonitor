6823227f6e5134740b6bdce87c75a8705f4dac5c
Edwin Perez <Edwin.Perez@blackboard.com>
2020-06-15 09:27:32 -0400

    implementing app genral options

--- src/client/src/components/AppMenu.tsx
+++ src/client/src/components/AppMenu.tsx
@@ -1,10 +1,25 @@
-import React, { ReactElement } from 'react'
-import { AppBar, Toolbar, makeStyles, Theme, createStyles } from '@material-ui/core'
+import React, { ReactElement, useState } from 'react'
+import { AppBar, BottomNavigation, BottomNavigationAction, makeStyles, Theme, createStyles, Divider } from '@material-ui/core'
+import { NetworkCheckSharp, SettingsBrightness, AccountTreeSharp  } from "@material-ui/icons";
+import ConfigPeek from "./ConfigPeek";
+
+interface IActiveAction {
+    selected?: string;
+    isDrawerOpen: boolean;
+}
 
 interface Props {}
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
+        root: {
+            color: theme.palette.text.primary,
+   
+            "&$selected": {
+                color: theme.palette.secondary.light
+            }
+        },
+        selected: {},
         appBar: {
             top: 'auto',
             bottom: 0,
@@ -14,11 +29,23 @@ const useStyles = makeStyles((theme: Theme) =>
 
 export default function AppMenu({ }: Props): ReactElement {
     const classes = useStyles();
+    const [activeAction, setActiveAction] = useState<IActiveAction>({isDrawerOpen: false});
+    const navActions = (event: React.ChangeEvent<{}>, newValue: string): void => {
+        if(newValue) {
+            setActiveAction({selected: newValue, isDrawerOpen: true});
+        }
+    }
     return (
-        <AppBar position="fixed" color="primary" className={classes.appBar}>
-            <Toolbar>
-
-            </Toolbar>
-        </AppBar>
+        <React.Fragment key='holder'>
+            <ConfigPeek isOpen={activeAction.isDrawerOpen} drawerType={activeAction.selected} />
+            <AppBar position="fixed" color="primary" className={classes.appBar}>
+                <Divider />
+                <BottomNavigation value={activeAction.selected} onChange={navActions} showLabels={true} classes={classes}>
+                    <BottomNavigationAction label="Monitors" value="monitorConfig" icon={<NetworkCheckSharp />} classes={classes} />
+                    <BottomNavigationAction label="System" value="sysConfig" icon={<SettingsBrightness />} classes={classes} />
+                    <BottomNavigationAction label="Integration" value="intConfig" icon={<AccountTreeSharp />} classes={classes} />
+                </BottomNavigation>
+            </AppBar>
+        </ React.Fragment>
     )
 }
