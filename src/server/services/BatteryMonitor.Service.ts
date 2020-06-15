90df9d5940b1a67d885ccfa91034f9fc28f6ddc7
Edwin Perez <Edwin.Perez@blackboard.com>
2020-06-15 09:27:02 -0400

    Increase loop count.

--- src/server/services/BatteryMonitor.Service.ts
+++ src/server/services/BatteryMonitor.Service.ts
@@ -84,8 +84,8 @@ export default class BatteryMonitor implements IBaterryMonitorService {
         if(sentCall == this.activeCall){
             // increase the loopcount to capture number of tries.
             loopCount++;
-            // we consider 2000 loops a hanging call. It will execute the call again.
-            if(loopCount > 2000){
+            // we consider 000 loops a hanging call. It will execute the call again.
+            if(loopCount > 4000){
                 console.log('Maximum loops reached. Requesting monitor info again:', sentCall);
                 this.getMonitorInfo(sentCall.address, sentCall.REG);
             } else {
